b517470aa913abd0359228561c91252062308c12
Kaur <Kaursamuel.kannel@gmail.com>
2019-03-11 14:48:56 +0200

    minecraft 2

--- test/e2e/EESTI_EKSPRESS_Minecraft.js
+++ test/e2e/EESTI_EKSPRESS_Minecraft.js
@@ -2,25 +2,26 @@ var config = require('../../nightwatch.conf.BASIC.js');
 
 module.exports = {
   '@tags': ['ekspress'],
-  'Eesti Ekspress to search for "minecraft" and click minecraft article': function(browser) {
+  'Eesti Ekspressist otsib minecrafti': function(browser) {
     browser
         .resizeWindow(1280, 1200)
         .url('https://ekspress.delfi.ee/')
-        .pause(100)
+        .pause(200)
         .waitForElementVisible('body')
         .pause(100)
         .setValue('.header-search__input', "minecraft")
-        .pause(100)
+        .pause(500)
         .click('div[class="header-search"] form button')
-        .pause(100)
-        .waitForElementVisible('body')
-        .saveScreenshot(`${config.imgpath(browser)}search-form-Minecraft.png`)
-        .pause(100)
-        .click('a[href="/areen/maailma-sees-on-veel-palju-suurem-maailm-ja-selle-nimi-on-minecraft?id=81416493"]')
         .pause(500)
         .waitForElementVisible('body')
+        .saveScreenshot(`${config.imgpath(browser)}Minecraft.png`)
+        .pause(200)
+        .click('a[href="/areen/maailma-sees-on-veel-palju-suurem-maailm-ja-selle-nimi-on-minecraft?id=81416493"]')
+        .pause(500)
         .saveScreenshot(`${config.imgpath(browser)}open-article.png`)
-        .pause(100)
+        .pause(900)
         .end();
   },
 };
+
+//class="pyfe-teaser-box-title"
